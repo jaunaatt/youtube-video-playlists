ac103e2f8c6ddceb8956b5b1076992b52332f1ee
LCRL-ULT <laycarrol936@gmail.com>
2025-12-26 19:06:10 +0700

    Main

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ int main() {
             
             adrPlaylist newPlaylist = createElementPlaylist(name, id);
             addPlaylist(L, newPlaylist);
-            
+            cout << "Playlist created successfully!" << endl;
             break;
             }
 
